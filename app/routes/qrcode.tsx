1f1e7e361564b46361533964cb87ff3c2291f09d
Daniel Fraga <dan.fraga33@gmail.com>
2024-10-31 22:35:24 +0000

    fix: change formLink

--- app/routes/qrcode.tsx
+++ app/routes/qrcode.tsx
@@ -2,12 +2,12 @@ import { Link } from '@remix-run/react';
 import { QRCodeSVG } from 'qrcode.react';
 
 function QRCodeRedirect() {
-	const formLink = 'http://localhost:5173/dashboard';
+	const formLink = 'http://localhost:5173/form';
 	return (
 		<div className="flex justify-center items-center h-screen flex-col gap-8">
 			<Link to="/">Home</Link>
 			<h1>Scan the QR Code to Access the Form</h1>
-			<QRCodeSVG value={formLink} />,
+			<QRCodeSVG value={formLink} />
 			<p>Open your camera and scan the QR code to be redirected to the form.</p>
 		</div>
 	);
