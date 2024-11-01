c6c53e3ba3785ab66ae5e0e8dc88570fa6e7b4f5
Daniel Fraga <dan.fraga33@gmail.com>
2024-11-01 08:08:28 +0000

    chore: remove console.log on DB connect

--- app/utils/db.ts
+++ app/utils/db.ts
@@ -5,7 +5,6 @@ export async function connectDB() {
 		await mongoose.connect(
 			'mongodb+srv://danfraga33:Z1VFYIiGe2AVPL7D@cluster0.1qjpo.mongodb.net/CustomerData?retryWrites=true&w=majority&appName=Cluster0'
 		);
-		console.log(process.env.MONGODB_URI);
 		console.log('Connection state:', mongoose.connection.readyState);
 	} catch (error) {
 		console.log(error);
