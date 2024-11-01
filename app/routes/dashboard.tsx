103d3465ad686162ce4928add86ab3e1938d1e08
Daniel Fraga <dan.fraga33@gmail.com>
2024-11-01 08:08:04 +0000

    fix; typing error with Customer Model

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,12 +1,12 @@
 import { Link, useLoaderData } from '@remix-run/react';
 import { json } from '@remix-run/node';
 import { connectDB } from '../utils/db';
-import Customers from '~/Models/Customer';
+import Customer from '~/Models/Customer';
 
 export async function loader() {
 	try {
 		connectDB();
-		const data = await Customers.find();
+		const data = await Customer.find();
 		console.log(data);
 		return json(data, { status: 200 });
 	} catch (err) {
@@ -17,21 +17,23 @@ export async function loader() {
 
 const Dashboard = () => {
 	const data = useLoaderData<typeof loader>();
+	console.log(data);
 
+	// Add delete functionality
 	return (
-		<div className="flex justify-center items-center h-screen  flex-col gap-8 mx-auto">
+		<div className="flex justify-center items-center h-screen  flex-col">
 			<Link to="/">Home</Link>
 			<h1>Data</h1>
-			<h2>
-				Customers:
+			<h2>Customers:</h2>
+
+			<div className="flex flex-col justify-between gap-2">
 				{data.map((customer) => (
-					<div className="flex justify-between gap-2" key={customer._id}>
-						<p key={customer._id}>
-							{customer.name}: {customer.email}
-						</p>
+					<div key={customer._id}>
+						<div>{customer.name}</div>
+						<div>{customer.email}</div>
 					</div>
 				))}
-			</h2>
+			</div>
 		</div>
 	);
 };
