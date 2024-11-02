270f47aadb12d938d4ac5a4c5ae22e318789fd4d
Daniel Fraga <dan.fraga33@gmail.com>
2024-11-02 14:58:42 +0000

    feat: add total revenue in dashboard

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -17,22 +17,39 @@ export async function loader() {
 
 const Dashboard = () => {
 	const data = useLoaderData<typeof loader>();
-	console.log(data);
+	let totalRevenue = data.reduce(
+		(total: number, customer) => total + customer.amount,
+		0
+	);
 
 	// Add delete functionality
 	return (
 		<div className="flex justify-center items-center h-screen  flex-col">
-			<Link to="/">Home</Link>
-			<h1>Data</h1>
-			<h2>Customers:</h2>
-
 			<div className="flex flex-col justify-between gap-2">
+				<Link to="/" className="flex justify-center item">
+					Home
+				</Link>
+				<h1 className="flex justify-center item">Data</h1>
+				<h2 className="flex justify-center item">Customers:</h2>
 				{data.map((customer) => (
 					<div key={customer._id}>
 						<div>{customer.name}</div>
 						<div>{customer.email}</div>
+						<label htmlFor="purchased" className="mr-2">
+							Purchased
+						</label>
+						{customer.Purchased ? (
+							<input type="checkbox" id="purchased" checked readOnly />
+						) : (
+							<input type="checkbox" id="purchased" readOnly />
+						)}
+
+						<p>
+							Amount Purchased: <span> {customer.amount}</span>
+						</p>
 					</div>
 				))}
+				<h3>Total Campaign Revenue: {totalRevenue}</h3>
 			</div>
 		</div>
 	);
