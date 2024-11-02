38256a9580f73be4fe3de53214458551f6cfa30b
Daniel Fraga <dan.fraga33@gmail.com>
2024-11-02 14:58:12 +0000

    chore: add form link in QR code for WIP speed

--- app/routes/qrcode.tsx
+++ app/routes/qrcode.tsx
@@ -6,6 +6,7 @@ function QRCodeRedirect() {
 	return (
 		<div className="flex justify-center items-center h-screen flex-col gap-8">
 			<Link to="/">Home</Link>
+			<Link to="/Form">Form</Link>
 			<h1>Scan the QR Code to Access the Form</h1>
 			<QRCodeSVG value={formLink} />
 			<p>Open your camera and scan the QR code to be redirected to the form.</p>
