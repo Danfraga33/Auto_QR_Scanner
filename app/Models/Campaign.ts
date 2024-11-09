564a169fb75f224841e617c6a84c93dfbc6ceeb6
Daniel Fraga <dan.fraga33@gmail.com>
2024-11-09 20:38:45 +0200

    chore: add startDate and endDate fields in schema

--- app/Models/Campaign.ts
+++ app/Models/Campaign.ts
@@ -3,6 +3,8 @@ import mongoose from 'mongoose';
 const campaignSchema = new mongoose.Schema({
 	name: { type: String, required: true },
 	campaignType: { type: String, required: true },
+	startDate: { type: String, required: true },
+	endDate: { type: String, required: true },
 	date: { type: String, required: true },
 	method: { type: String, required: true },
 });
