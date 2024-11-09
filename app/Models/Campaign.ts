a2a24b4a0c3e8428b556f40ff0f0979661cb90d3
Daniel Fraga <dan.fraga33@gmail.com>
2024-11-09 19:42:25 +0200

    feat: add campaign model

---
+++ app/Models/Campaign.ts
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const campaignSchema = new mongoose.Schema({
+	name: { type: String, required: true },
+	campaignType: { type: String, required: true },
+	date: { type: String, required: true },
+	method: { type: String, required: true },
+});
+
+export default mongoose.models?.Campaign ||
+	mongoose.model('Campaign', campaignSchema);
