c1033d7c6e6d58458561bfbc29444f7d9f1b2487
Daniel Fraga <dan.fraga33@gmail.com>
2024-11-11 15:41:07 +0200

    chore: update landing page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
 import type { MetaFunction } from '@remix-run/node';
 import { Link } from '@remix-run/react';
+import { authenticator } from '~/services/auth.server';
 
 export const meta: MetaFunction = () => {
 	return [
@@ -10,9 +11,42 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
 	return (
-		<>
-			<Link to="/Dashboard">Dashboard</Link>
-			<Link to="/form">Form</Link>
-		</>
+		<div className="flex justify-center h-screen items-center flex-col">
+			<Link to="/Dashboard" className="items-start">
+				Dashboard
+			</Link>
+			<Link to="/form" className="items-start">
+				Form
+			</Link>
+			<Link to="/campaignBuilder" className="items-start">
+				Campaign Builder
+			</Link>
+			<Link to="/login" className="items-start">
+				Signup
+			</Link>
+		</div>
 	);
+}
+
+let user = await authenticator.isAuthenticated(request, {
+	failureRedirect: '/login',
+});
+
+// if the user is authenticated, redirect to /dashboard
+let user = await authenticator.isAuthenticated(request, {
+	failureRedirect: '/login',
+});
+
+// if the user is authenticated, redirect to /dashboard
+await authenticator.isAuthenticated(request, {
+	successRedirect: '/dashboard',
+});
+
+// get the user or null, and do different things in your loader/action based on
+// the result
+user = await authenticator.isAuthenticated(request);
+if (user) {
+	// here the user is authenticated
+} else {
+	// here the user is not authenticated
 }
