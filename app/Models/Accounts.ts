1bc69b4e14dad47c0ca90e483886121004bc7204
Daniel Fraga <dan.fraga33@gmail.com>
2024-11-17 11:08:27 +0200

    fix: edit acounts modelinputs

--- app/Models/Accounts.ts
+++ app/Models/Accounts.ts
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
 
 const accountsSchema = new mongoose.Schema({
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
 	email: { type: String, required: true },
-	password: { type: String, required: true },
+	password: { type: String },
 	companyName: { type: String },
 	companyEmail: { type: String },
 	companyPhoneNumber: { type: String },
