b84ec01fda70a93d91ba86d8e5d76e05a154ca43
Daniel Fraga <dan.fraga33@gmail.com>
2024-11-27 15:42:52 +1100

    feat: add calender comp

---
+++ app/Components/SelectedCampaign.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Calendar } from "./ui/calendar";
+
+const SelectedCampaign = () => {
+  return (
+    <div>
+      <Calendar />
+    </div>
+  );
+};
+
+export default SelectedCampaign;
