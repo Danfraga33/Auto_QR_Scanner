fad82e6535468c98ef0c5ae474ded793ab7a1368
Daniel Fraga <dan.fraga33@gmail.com>
2024-11-28 13:12:28 +1100

    fix: add Link tags for sidebar company list

--- app/Components/CompanyName.tsx
+++ app/Components/CompanyName.tsx
@@ -1,5 +1,6 @@
 import { LayoutDashboard } from "lucide-react";
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "./ui/sidebar";
+import { Link } from "@remix-run/react";
 
 export default function CompanyName() {
   const company = { name: "Acme Inc", logo: LayoutDashboard };
@@ -10,7 +11,9 @@ export default function CompanyName() {
           <div className="flex aspect-square size-5 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
             <company.logo className="size-3" />
           </div>
-          <span className="truncate font-semibold">{company.name}</span>
+          <Link to="/">
+            <span className="truncate font-semibold">{company.name}</span>
+          </Link>
         </SidebarMenuButton>
       </SidebarMenuItem>
     </SidebarMenu>
