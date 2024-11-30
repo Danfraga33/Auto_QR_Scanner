36fb9d5e4d9fa62ec26e3b3bbff4eebde81612de
Daniel Fraga <dan.fraga33@gmail.com>
2024-11-30 16:53:14 +1100

    fix: adjust campaign model

--- app/Models/Campaign.ts
+++ app/Models/Campaign.ts
@@ -2,10 +2,9 @@ import mongoose from "mongoose";
 
 const campaignSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  campaignType: { type: String, required: true },
   startDate: { type: String, required: true },
-  endDate: { type: String, required: true },
-  method: { type: String, required: true },
+  freq: { type: String, required: true },
+  method: { type: String, required: true, default: "Email" },
 });
 
 export default mongoose.models?.Campaign ||
