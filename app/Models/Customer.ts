b780f1b82c877ecae75ff0639962ef165ffdead2
Daniel Fraga <dan.fraga33@gmail.com>
2024-12-04 12:38:11 +1100

    style: prettier marginizing

--- app/Models/Customer.ts
+++ app/Models/Customer.ts
@@ -1,15 +1,15 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const customerSchema = new mongoose.Schema(
-	{
-		name: { type: String, required: true },
-		email: { type: String, required: true },
-		purchased: { type: Boolean, default: false },
-		amountPurchased: { type: Number, default: 0.0 },
-		phone: String,
-	},
-	{ collection: 'Customers' }
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    purchased: { type: Boolean, default: false },
+    amountPurchased: { type: Number, default: 0.0 },
+    phone: String,
+  },
+  { collection: "Customers" },
 );
 
 export default mongoose.models?.Customer ||
-	mongoose.model('Customer', customerSchema);
+  mongoose.model("Customer", customerSchema);
