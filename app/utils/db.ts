a55e374836c59853b228ec09af1be42671c965f5
Daniel Fraga <dan.fraga33@gmail.com>
2024-12-05 16:20:13 +1100

    chore: add descriptive fail connect message for db

--- app/utils/db.ts
+++ app/utils/db.ts
@@ -1,12 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export async function connectDB() {
-	try {
-		await mongoose.connect(
-			'mongodb+srv://danfraga33:Z1VFYIiGe2AVPL7D@cluster0.1qjpo.mongodb.net/CustomerData?retryWrites=true&w=majority&appName=Cluster0'
-		);
-		console.log('Connection state:', mongoose.connection.readyState);
-	} catch (error) {
-		console.log(error);
-	}
+  try {
+    await mongoose.connect(
+      "mongodb+srv://danfraga33:Z1VFYIiGe2AVPL7D@cluster0.1qjpo.mongodb.net/CustomerData?retryWrites=true&w=majority&appName=Cluster0",
+    );
+    console.log("Connection state:", mongoose.connection.readyState);
+  } catch (error) {
+    console.log({
+      success: false,
+      message: "Failed to connect to mongoDB",
+      error,
+    });
+  }
 }
