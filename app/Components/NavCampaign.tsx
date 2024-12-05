628975f52ee56ab99e0a4463dda3e19c10b22198
Daniel Fraga <dan.fraga33@gmail.com>
2024-12-05 16:40:28 +1100

    feat: add Navlink && use DB instead of Mock

--- app/Components/NavCampaign.tsx
+++ app/Components/NavCampaign.tsx
@@ -9,21 +9,29 @@ import {
 } from "./ui/sidebar";
 import { Badge } from "./ui/badge";
 import campaigns from "~/lib/data/campaigns.json";
-import { Link } from "@remix-run/react";
+import { NavLink, json, useLoaderData } from "@remix-run/react";
+import { getCampaign } from "~/utils/actions";
+import { LoaderFunction } from "@remix-run/node";
+
+export const loader: LoaderFunction = async () => {
+  const data = await getCampaign();
+  return json(data);
+};
 
 export default function NavCampaigns() {
+  const campaigns = useLoaderData<typeof loader>();
   return (
     <SidebarGroup>
       <SidebarGroupLabel>Campaigns</SidebarGroupLabel>
       <SidebarGroupContent>
         <SidebarMenu>
           {campaigns.map((campaign) => (
-            <SidebarMenuItem key={campaign.name}>
+            <SidebarMenuItem key={campaign.id}>
               <SidebarMenuButton asChild>
-                <Link to="/campaigns">
+                <NavLink to={`/campaigns/${campaign._id}`}>
                   <Target className="h-4 w-4" />
                   <span>{campaign.name}</span>
-                </Link>
+                </NavLink>
               </SidebarMenuButton>
               <Badge
                 variant={
