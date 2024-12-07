a7eabb131fac675f516fc8596357138517302e8d
Daniel Fraga <dan.fraga33@gmail.com>
2024-12-07 21:42:17 +1100

    feat: add custom auth flow WIP

--- app/routes/sign-in.$.tsx
+++ app/routes/sign-in.$.tsx
@@ -1,9 +1,13 @@
-import { SignIn } from "@clerk/remix";
+import { SignIn, useSignIn } from "@clerk/remix";
 import React from "react";
 
 const SignInPage = () => {
+  const data = useSignIn();
+  console.log(data);
+
   return (
     <div className="flex flex-col justify-center items-center h-screen">
+      <h1>Sign Up route</h1>
       <SignIn />
     </div>
   );
