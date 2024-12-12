6d53952cf5415d7a9b8c3bb2e38b2fe762becf09
Daniel Fraga <dan.fraga33@gmail.com>
2024-12-12 11:58:05 +1100

    style: change margin spacing

--- app/Components/LandingPage/Hero.tsx
+++ app/Components/LandingPage/Hero.tsx
@@ -3,8 +3,8 @@ import { Button } from "../ui/button";
 
 const Hero = () => {
   return (
-    <section className="flex items-center flex-col mx-12 w-full py-12 md:py-24 lg:py-32 xl:py-48">
-      <div className="container px-4 md:px-6">
+    <section id="hero" className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+      <div className="px-4 md:px-6">
         <div className="flex flex-col items-center space-y-6 text-center">
           <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
             Supercharge Your Marketing Campaigns
--- app/Components/LandingPage/Pricing.tsx
+++ app/Components/LandingPage/Pricing.tsx
@@ -11,11 +11,8 @@ import { CheckCircle } from "lucide-react";
 
 const Pricing = () => {
   return (
-    <section
-      id="pricing"
-      className="flex items-center flex-col mx-12 w-full py-12 md:py-24 lg:py-32"
-    >
-      <div className="container px-4 md:px-6">
+    <section id="pricing" className="w-full py-12 md:py-24 lg:py-32">
+      <div className="flex items-center flex-col mx-12 px-4 md:px-6">
         <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl text-center mb-12">
           Pricing Plans
         </h2>
