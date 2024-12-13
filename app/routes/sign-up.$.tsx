973de779ebd90b8c087a810333aa453436a6a5c8
Daniel Fraga <dan.fraga33@gmail.com>
2024-12-13 20:32:48 +1100

    feat: custom signup functioning

--- app/routes/sign-up.$.tsx
+++ app/routes/sign-up.$.tsx
@@ -1,8 +1,7 @@
 import { useSignUp } from "@clerk/remix";
-import { ActionFunctionArgs, redirect } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import { redirect, useNavigate } from "@remix-run/react";
 import { Computer, Github } from "lucide-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { Button } from "~/components/ui/button";
 import {
   Card,
@@ -21,15 +20,19 @@ const SignupPage = () => {
   const [showPassword, setShowPassword] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  const [PendingVerification, setPendingVerification] = useState(false);
+  const [pendingVerification, setPendingVerification] = useState(false);
+  const [verificationCode, setVerificationCode] = useState("");
 
   const handleNewUser = async ({
     email,
     password,
+    e,
   }: {
     email: string;
     password: string;
+    e: FormEvent<HTMLFormElement>;
   }) => {
+    e.preventDefault();
     try {
       const data = await signUp?.create({
         emailAddress: email,
@@ -41,12 +44,13 @@ const SignupPage = () => {
       });
 
       setPendingVerification(true);
-      redirect("/");
+
       return {
         message: "User Successfully Added",
         status: data,
       };
-    } catch (error) {
+    } catch (error: any) {
+      setError(error.errors[0].message);
       return {
         message: "Unable to create new client",
         error: error,
@@ -54,70 +58,119 @@ const SignupPage = () => {
     }
   };
 
+  async function onPressVerify(e: FormEvent<HTMLFormElement>, code: string) {
+    e.preventDefault();
+    if (!isLoaded) return;
+    const navigate = useNavigate();
+
+    try {
+      const completeSignup = await signUp.attemptEmailAddressVerification({
+        code,
+      });
+
+      if (completeSignup.status !== "complete") {
+        console.log(JSON.stringify(completeSignup));
+      }
+      if (completeSignup.status !== "complete") {
+        await setActive({ session: completeSignup.createdSessionId });
+        navigate("/");
+      }
+    } catch (error: any) {
+      console.log(JSON.stringify(error));
+      setError(error.error[0].message);
+    }
+  }
+
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <Card>
-        <CardHeader className="space-y-1">
-          <CardTitle className="text-2xl">Create an account</CardTitle>
-          <CardDescription>
-            Enter your email below to create your account
-          </CardDescription>
-        </CardHeader>
-        <form>
-          <CardContent className="grid gap-4">
-            <div className="grid grid-cols-2 gap-6">
-              <Button variant="outline">
-                <Github />
-                Github
-              </Button>
-              <Button variant="outline">
-                <Computer />
-                Google
-              </Button>
-            </div>
-            <div className="relative">
-              <div className="absolute inset-0 flex items-center">
-                <span className="w-full border-t" />
+      {!pendingVerification ? (
+        <Card>
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-2xl">Create an account</CardTitle>
+            <CardDescription>
+              Enter your email below to create your account
+            </CardDescription>
+          </CardHeader>
+          <form onSubmit={(e) => handleNewUser({ email, password, e })}>
+            <CardContent className="grid gap-4">
+              <div className="grid grid-cols-2 gap-6">
+                <Button variant="outline">
+                  <Github />
+                  Github
+                </Button>
+                <Button variant="outline">
+                  <Computer />
+                  Google
+                </Button>
               </div>
-              <div className="relative flex justify-center text-xs uppercase">
-                <span className="bg-background px-2 text-muted-foreground">
-                  Or continue with
-                </span>
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center">
+                  <span className="w-full border-t" />
+                </div>
+                <div className="relative flex justify-center text-xs uppercase">
+                  <span className="bg-background px-2 text-muted-foreground">
+                    Or continue with
+                  </span>
+                </div>
               </div>
-            </div>
 
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="hook@CRM.com"
-                name="email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                id="password"
-                type="password"
-                name="password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button
-              className="w-full"
-              onClick={() => handleNewUser({ email, password })}
-            >
-              Create account
-            </Button>
-          </CardFooter>
-        </form>
-        {error && <p className="text-red-500">{error}</p>}
-        {/* {success && <p className="text-green-500">{success}</p>} */}
-      </Card>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="hook@CRM.com"
+                  name="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  type="password"
+                  name="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full" type="submit">
+                Create account
+              </Button>
+            </CardFooter>
+          </form>
+          {error && <p className="text-red-500">{error}</p>}
+          {/* {success && <p className="text-green-500">{success}</p>} */}
+        </Card>
+      ) : (
+        <Card>
+          <CardHeader className="space-y-1">
+            <CardTitle className="text-2xl">Verify</CardTitle>
+            <CardDescription>Enter the code sent to your email</CardDescription>
+          </CardHeader>
+          <form onSubmit={(e) => onPressVerify(e, verificationCode)}>
+            <CardContent className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="code">Verification Code</Label>
+                <Input
+                  id="code"
+                  type="code"
+                  name="code"
+                  onChange={(e) => setVerificationCode(e.target.value)}
+                />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full" type="submit">
+                Submit
+              </Button>
+            </CardFooter>
+          </form>
+          {error && <p className="text-red-500">{error}</p>}
+          {/* {success && <p className="text-green-500">{success}</p>} */}
+        </Card>
+      )}
     </div>
   );
 };
