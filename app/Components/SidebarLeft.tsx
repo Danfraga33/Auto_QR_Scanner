2a3428206f7ae8e8458290fc2747589962669048
Daniel Fraga <dan.fraga33@gmail.com>
2024-12-13 20:35:45 +1100

    chore: remove unneded imports

--- app/Components/SidebarLeft.tsx
+++ app/Components/SidebarLeft.tsx
@@ -2,27 +2,32 @@ import CompanyName from "./CompanyName";
 import NavCampaigns from "./NavCampaign";
 import { NavMain } from "./NavMain";
 import NavUser from "./NavUser";
+import { Separator } from "./ui/separator";
 import {
   Sidebar,
   SidebarContent,
   SidebarHeader,
   SidebarRail,
 } from "./ui/sidebar";
-import marketingAppData from "../lib/data/marketingAppData.json";
 import { ComponentProps } from "react";
 
 function SidebarLeft({ ...props }: ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar className="border-r-0 bg-gray-200/50" {...props}>
+    <Sidebar
+      className="border-r-0 custom-gray rounded-tr-3xl shadow-lg"
+      {...props}
+    >
       <SidebarHeader>
         <CompanyName />
         <NavMain />
       </SidebarHeader>
+      <Separator role="contentinfo" />
       <SidebarContent>
         <NavCampaigns />
         {/* <NavLeads /> */}
         {/* <NavAnalytics /> */}
       </SidebarContent>
+      <Separator />
       <NavUser />
       <SidebarRail />
     </Sidebar>
