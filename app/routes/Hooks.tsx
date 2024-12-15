fb7d5be94f6f81a712bf9ee710a274f87627a0bb
Daniel Fraga <dan.fraga33@gmail.com>
2024-12-15 22:36:40 +1100

    feat: add Hooks page

--- app/routes/Hooks.tsx
+++ app/routes/Hooks.tsx
@@ -1,47 +1,73 @@
 import { ProjectCard } from "~/components/ProjectCard";
 import SidebarComp from "~/components/Sidebar";
 import { Separator } from "~/components/ui/separator";
-import Hooks from "~/lib/data/Hooks.json";
+import HooksData from "~/lib/data/Hooks.json";
 import UserHooks from "~/lib/data/UserHooks.json";
-import { Card, CardHeader } from "~/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import { Button } from "~/components/ui/button";
+import { Unplug } from "lucide-react";
+import { useToast } from "~/hooks/use-toast";
+import { ToastAction } from "~/components/ui/toast";
+import { CreateConnection } from "~/components/CreateConnection";
+import { redirect } from "@remix-run/node";
 
-const vDashboard = () => {
+export async function action() {
+  return redirect("/campaigns");
+}
+
+const Hooks = () => {
+  const { toast } = useToast();
+
   return (
     <SidebarComp>
-      <main className="grid  grid-cols-8 py-6 sm:px-6 lg:px-8">
-        <div className="col-span-6 max-w-7xl pr-8 ">
-          <div className="px-4 py-6 sm:px-0">
-            <h2 className="text-2xl font-semibold text-gray-800 dark:text-white mb-6">
-              Your Hooks
-            </h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-              {UserHooks.map((hook) => (
-                <ProjectCard key={hook.id} {...hook} />
-              ))}
+      <main className="grid grid-cols-8 py-6 sm:px-6 lg:px-8">
+        <div className="col-span-6 max-w-7xl pr-8">
+          <div className="flex">
+            <div className="px-4 py-6 sm:px-0">
+              <h2 className="mb-6 text-2xl font-semibold text-gray-800 dark:text-white">
+                Your Hooks
+              </h2>
+              <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                {UserHooks.map((hook) => (
+                  <ProjectCard key={hook.id} {...hook} />
+                ))}
+              </div>
             </div>
+            <CreateConnection />
           </div>
+
           <Separator />
           <div className="px-4 py-6 sm:px-0">
-            <h2 className="text-2xl font-semibold text-gray-800 dark:text-white mb-6">
+            <h2 className="mb-6 text-2xl font-semibold text-gray-800 dark:text-white">
               Hook Options
             </h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-              {Hooks.map((hook) => (
+            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+              {HooksData.map((hook) => (
                 <ProjectCard key={hook.id} {...hook} />
               ))}
             </div>
           </div>
         </div>
         <div className="col-span-2">
-          <div className="gap-2 flex flex-col">
+          <div className="flex flex-col gap-2">
             <Card>
-              <CardHeader className="w-full">Most Used Hook</CardHeader>
+              <CardHeader>
+                <CardTitle>Most Used Hook</CardTitle>
+              </CardHeader>
+              <CardContent className="flex flex-col items-center justify-center">
+                <p className="text-sm">QR Scanner</p>
+                <div className="text-4xl font-bold">73</div>
+              </CardContent>
             </Card>
             <Card className="col-span-2">
-              <CardHeader className="w-full">Most Used Hook</CardHeader>
-            </Card>
-            <Card className="col-span-2">
-              <CardHeader className="w-full">Most Used Hook</CardHeader>
+              <CardHeader className="w-full">
+                Highest Converting Hook
+              </CardHeader>
+
+              <CardContent className="flex flex-col items-center justify-center">
+                <p className="text-sm">Email</p>
+                <div className="text-4xl font-bold">23%</div>
+              </CardContent>
             </Card>
           </div>
         </div>
@@ -50,4 +76,4 @@ const vDashboard = () => {
   );
 };
 
-export default vDashboard;
+export default Hooks;
