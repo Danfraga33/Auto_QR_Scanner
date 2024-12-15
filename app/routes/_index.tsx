ea2aee5ce160e69b43b21437e4fb86288bf1aeff
Daniel Fraga <dan.fraga33@gmail.com>
2024-12-15 22:38:26 +1100

    fix: remove unneeded imports

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,13 +1,4 @@
 import { type MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
-import {
-  ArrowRight,
-  BarChart2,
-  CheckCircle,
-  Target,
-  Users,
-} from "lucide-react";
-import AuthComponent from "~/components/AuthComponent";
 import CTA from "~/components/LandingPage/CTA";
 import Features from "~/components/LandingPage/Features";
 import Footer from "~/components/LandingPage/Footer";
@@ -16,15 +7,6 @@ import Hero from "~/components/LandingPage/Hero";
 import Pricing from "~/components/LandingPage/Pricing";
 import Testimonials from "~/components/LandingPage/Testimonials";
 
-import { Button } from "~/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "~/components/ui/card";
-import { Input } from "~/components/ui/input";
 export const meta: MetaFunction = () => {
   return [
     { title: "QR Scanner" },
@@ -35,7 +17,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <>
-      <div className="flex flex-col min-h-screen">
+      <div className="flex min-h-screen flex-col">
         <Header />
 
         <main className="flex-1">
